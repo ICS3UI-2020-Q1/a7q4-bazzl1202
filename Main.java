9cb35c8199cf6d3b7211b8972674c1ae291a9464
bazzl1202 <bazzl1202@wrdsb.ca>
2020-10-14 00:37:54 +0000

    Now gives you last digit of a number

--- Main.java
+++ Main.java
@@ -1,15 +1,24 @@
 /**
- *
- * @author 
+ * A method that gives you the last digit of a number
+ * @author Liana Bazzarella
  */
 public class Main {
 
+  // method that gives you last digit of number
+  public static double lastDigit(double integer){
+    double answer = ( integer % 10);
+    if(integer < 0){
+      answer = ( -integer % 10);
+    }
+    System.out.println(answer);
+    return answer;
+  }
   /**
    * The method that is executed when you hit the run button.
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    // number used for finding last digit
+    lastDigit(1459);
   }
 }
